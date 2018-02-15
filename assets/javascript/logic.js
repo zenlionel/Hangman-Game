1403c4416ee668f1d5cc6b232478de5fa969e3e3
Leo <zenlionel@gmail.com>
2018-02-14 16:14:21 -0800

    check for letters

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -59,3 +59,38 @@ function startGame() {
   document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
 }
 
+
+function checkLetters(letter) {
+
+  // Toggle if letter is in word.
+  var letterInWord = false;
+
+  // Check if a letter exists inside the array at all.
+  for (var i = 0; i < numBlanks; i++) {
+    if (chosenWord[i] === letter) {
+      letterInWord = true;
+    }
+  }
+
+  // If the letter exists somewhere in the word, then figure out exactly where (which indices).
+  if (letterInWord) {
+
+    // Loop through the word.
+    for (var j = 0; j < numBlanks; j++) {
+
+      // Populate the blanksAndSuccesses with every instance of the letter.
+      if (chosenWord[j] === letter) {
+        // Here we set the specific space in blanks and letter equal to the letter when there is a match.
+        blanksAndSuccesses[j] = letter;
+      }
+    }
+    // Logging for testing.
+    console.log(blanksAndSuccesses);
+  }
+  // If the letter doesn't exist at all...
+  else {
+    // ..then we add the letter to the list of wrong letters, and we subtract one of the guesses.
+    wrongGuesses.push(letter);
+    numGuesses--;
+  }
+}
